2a3fdd2ac8f08a84ff59b738f2f2117ead420700
omkarrrJoshi <one.omkarjoshi@gmail.com>
2025-02-08 14:21:06 +0530

    nit changes

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./src/app");
 const config = require('./src/config/config')
 
 app.listen(config.port, () => {
-  console.log(`Server running in ${config.env} mode at ${config.hostUrl}`);
+  console.log(`Server running in ${config.env} mode at ${config.hostUrl} and port ${config.port}`);
 });
