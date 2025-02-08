d2bec4270d2598ed6c93353ad4514b70de4ac950
omkarrrJoshi <one.omkarjoshi@gmail.com>
2025-02-08 16:49:21 +0530

    db connection setup

---
+++ migrations/20250208102226_create_daily_task_ref.js
@@ -0,0 +1,21 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("daily_task_ref", (table) => {
+    table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()")); // Primary key
+    table.uuid("user_id").notNullable(); // Foreign key reference to users
+    table.string("name").notNullable();
+    table.string("pillar").notNullable();
+    table.string("type").notNullable();
+    table.specificType("target", "INTEGER[7]").notNullable(); // Array of 7 integers
+    table.string("link").nullable();
+    table.text("description").nullable();
+    table.timestamp("start_date").notNullable();
+    table.timestamp("end_date").nullable();
+    table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("deleted_at").nullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("daily_task_ref");
+};
